7557f0b401350d8f0f7f159e71a307dffe338e70
Yiğit Malik <yigitmalikuzuntas@gmail.com>
2023-12-16 01:37:03 +0300

    newwebsite

---
+++ src/components/Blog/BlogSection.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import "./Blog.scss";
+import SectionHeading from '../SectionHeading/SectionHeading';
+import Carousel from '../Slider/Carousel';
+
+const Blog = ({ data }) => {
+    return (
+        true
+  )
+}
+
+Blog.propTypes = {
+  variant: PropTypes.string,
+  data: PropTypes.object
+}
+
+export default Blog
---
+++ src/components/Iconbox/Iconbox.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import SectionHeading from '../SectionHeading/SectionHeading'
+import './Iconbox.scss';
+
+const Iconbox = ({ data }) => {
+  const { services } = data;
+    return (true
+
+  )
+}
+
+Iconbox.propTypes = {
+  data: PropTypes.object
+}
+
+export default Iconbox;
